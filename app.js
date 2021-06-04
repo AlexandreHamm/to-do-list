f5a74075373e5d4af1cfa18d7870cfd3b604f3a6
ahamm-prequal2021 <a.hamm@codeur.online>
2021-06-04 15:32:29 +0200

    feat: Storage + Clear button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let enterButton = document.getElementById('enter');
+let clearButton = document.getElementById('clear');
 let input = document.getElementById('userInput');
 let ul = document.querySelector('ul');
 let item = document.getElementsByTagName('li');
@@ -11,27 +12,32 @@ function createListElement(){
     let li = document.createElement('li');
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    storage();
     input.value = '';
 
     function crossOut(){
         li.classList.toggle('done');
+        storage();
     }
-    
     li.addEventListener('click', crossOut);
+    
     
     let dBtn = document.createElement('button');
     dBtn.appendChild(document.createTextNode('X'));
     li.appendChild(dBtn);
-    dBtn.addEventListener('click', deleteListItem);
+    storage();
+    dBtn.addEventListener('click', () => {
+        li.remove();
+    });
+    storage();
     
-    function deleteListItem(){
-        li.classList.add('delete');
-    }
 }
 
 function addListAfterClick(){
     if(inputLength() > 0){
         createListElement();
+    } else {
+        alert('Le champ est vide.');
     }
 }
 
@@ -42,5 +48,24 @@ function addListAfterKeypress(event){
 }
 
 enterButton.addEventListener('click', addListAfterClick);
+clearButton.addEventListener('click', () => {
+    window.localStorage.clear();
+    ul.innerHTML = '';
+});
 
 input.addEventListener('keypress', addListAfterKeypress);
+
+// stockage
+function storage(){
+    window.localStorage.todoList = ul.innerHTML;
+}
+function getValues(){
+    let storageContent = window.localStorage.todoList;
+    if(!storageContent){
+        ul.innerHTML = '';
+    }
+    else{
+        ul.innerHTML = storageContent;
+    }
+}
+getValues();
